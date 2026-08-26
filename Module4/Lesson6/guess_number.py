8693f6dd636fc8670a94b8e068cd86a2d7080d92
SurbhiRijhwani <surbhirijhwani5@gmail.com>
2026-08-26 11:44:12 +0530

    indentation proper

--- Module4/Lesson6/guess_number.py
+++ Module4/Lesson6/guess_number.py
@@ -1,263 +1,89 @@
-# Import necessary modules
-
 import random
-
 import time
 
 
-# Pick a number between 1 and 100
-
-number=random.randint(1, 100)
-
-
 def intro():
+    print("May I ask you for your name?")
 
-print("May I ask you for your name?")
+    global name
+    name = input()
 
-# declaring name variable global so it can be accessed outside the function
+    print(
+        name
+        + ", we are going to play a game. I am thinking of a number between 1 and 100."
+    )
 
-global name
+    if number % 2 == 0:
+        x = "even"
+    else:
+        x = "odd"
 
-name = input() #asks for the name
+    print("\nThis is an {} number.".format(x))
 
-print(name + ", we are going to play a game. I am thinking of a number between 1 and 100")
-
-if(number%2==0):
-
-x='even'
-
-else:
-
-x='odd'
-
-print("\nThis is an {} number".format(x))
-
-time.sleep(.5)
-
-print("Go ahead. Guess!")
+    time.sleep(0.5)
+    print("Go ahead. Guess!")
 
 
 def pick():
+    guessesTaken = 0
 
-guessesTaken = 0
+    # The number of guesses is limited to 6
+    while guessesTaken < 6:
+        time.sleep(0.25)
 
+        enter = input("Guess: ")
 
-#if the number of guesses is less than 6
+        try:
+            # Convert the input into an integer
+            guess = int(enter)
 
-while guessesTaken < 6:
+            # Check if the guess is in range
+            if guess >= 1 and guess <= 100:
+                guessesTaken += 1
 
-time.sleep(.25)
+                if guess < number:
+                    print("The number you entered is too low.")
 
-#inserts the place to enter guess
+                elif guess > number:
+                    print("The number you entered is too high.")
 
-enter=input("Guess: ")
+                else:
+                    # Correct guess
+                    break
 
+                if guessesTaken < 6:
+                    time.sleep(0.5)
+                    print("Try Again!")
 
-#check if a number was entered
+            else:
+                print("Silly Goose! That number isn't in the range!")
+                time.sleep(0.25)
+                print("Please enter a number between 1 and 100.")
 
-try:
+        except ValueError:
+            print("I don't think that " + enter + " is a number. Sorry!")
 
+    # Check whether the player guessed correctly
+    if guess == number:
+        print(
+            "Good job, {}! You guessed my number in {} guesses!".format(
+                name, guessesTaken
+            )
+        )
+    else:
+        print("Nope. The number I was thinking of was " + str(number))
 
-#stores the guess as an integer instead of a string
 
-guess = int(enter)    
+# Main game loop
+playagain = "yes"
 
+while playagain.lower() in ["yes", "y"]:
 
-if guess<=100 and guess>=1: #if they are in range
+    # Generate a new number for every new game
+    number = random.randint(1, 100)
 
-guessesTaken=guessesTaken+1 #adds one guess each time the player is wrong
+    intro()
+    pick()
 
-if guessesTaken<6:
-
-if guess<number:
-
-print("The guess of the number that you have entered is too low")
-
-if guess>number:
-
-print("The guess of the number that you have entered is too high")
-
-if guess != number:
-
-time.sleep(.5)
-
-print("Try Again!")
-
-
-#if the guess is right, then we are going to jump out of the while block
-
-if guess==number:
-
-break
-
-
-
-if guess>100 or guess<1:
-
-print("Silly Goose! That number isn't in the range!")
-
-time.sleep(.25)
-
-print("Please enter a number between 1 and 100")
-
-
-except: #if a number wasn't entered
-
-print("I don't think that "+enter+" is a number. Sorry")
-
-
-if guess == number:
-
-guessesTaken = str(guessesTaken)
-
-print('Good job, {}! You guessed my number in {} guesses!'.format(name, guessesTaken))
-
-
-if guess != number:
-
-print('Nope. The number I was thinking of was ' + str(number))
-
-
-playagain="yes"
-
-while playagain=="yes" or playagain=="y" or playagain=="Yes":
-
-intro()
-
-pick()
-
-print("Do you want to play again?")
-
-playagain=input()
-
-# Import necessary modules
-
-import random
-
-import time
-
-
-# Pick a number between 1 and 100
-
-number=random.randint(1, 100)
-
-
-def intro():
-
-print("May I ask you for your name?")
-
-# declaring name variable global so it can be accessed outside the function
-
-global name
-
-name = input() #asks for the name
-
-print(name + ", we are going to play a game. I am thinking of a number between 1 and 100")
-
-if(number%2==0):
-
-x='even'
-
-else:
-
-x='odd'
-
-print("\nThis is an {} number".format(x))
-
-time.sleep(.5)
-
-print("Go ahead. Guess!")
-
-
-def pick():
-
-guessesTaken = 0
-
-
-#if the number of guesses is less than 6
-
-while guessesTaken < 6:
-
-time.sleep(.25)
-
-#inserts the place to enter guess
-
-enter=input("Guess: ")
-
-
-#check if a number was entered
-
-try:
-
-
-#stores the guess as an integer instead of a string
-
-guess = int(enter)    
-
-
-if guess<=100 and guess>=1: #if they are in range
-
-guessesTaken=guessesTaken+1 #adds one guess each time the player is wrong
-
-if guessesTaken<6:
-
-if guess<number:
-
-print("The guess of the number that you have entered is too low")
-
-if guess>number:
-
-print("The guess of the number that you have entered is too high")
-
-if guess != number:
-
-time.sleep(.5)
-
-print("Try Again!")
-
-
-#if the guess is right, then we are going to jump out of the while block
-
-if guess==number:
-
-break
-
-
-
-if guess>100 or guess<1:
-
-print("Silly Goose! That number isn't in the range!")
-
-time.sleep(.25)
-
-print("Please enter a number between 1 and 100")
-
-
-except: #if a number wasn't entered
-
-print("I don't think that "+enter+" is a number. Sorry")
-
-
-if guess == number:
-
-guessesTaken = str(guessesTaken)
-
-print('Good job, {}! You guessed my number in {} guesses!'.format(name, guessesTaken))
-
-
-if guess != number:
-
-print('Nope. The number I was thinking of was ' + str(number))
-
-
-playagain="yes"
-
-while playagain=="yes" or playagain=="y" or playagain=="Yes":
-
-intro()
-
-pick()
-
-print("Do you want to play again?")
-
-playagain=input()
+    print("Do you want to play again?")
+    playagain = input()
